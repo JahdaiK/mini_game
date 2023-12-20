49c1561fb977174ba5e897c56bdba3ac4678e644
Jahdai <jahdai.kilkenny@gmail.com>
2023-12-20 09:02:59 -0500

    created gameboard and keyboard

---
+++ app.js
@@ -0,0 +1,7 @@
+/* Wordle functionalities:
+1. Store words: 
+    2 groups: Solutions (game winning words) and Bank(a bank of 5 letter words)
+2. Identify letters that are in the winning words:
+    -gray if not in word, -yellow if in word but wrong spot, -green if in word and right spot
+3. Keyboard that will allow you to place letters into boxes, delete, press enter
+4. keyboard must also reflect the colors of tiles (gray, green, yellow)
