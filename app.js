1734aee684f9251fe078704c176c9a35321743d6
Jahdai <jahdai.kilkenny@gmail.com>
2023-12-21 15:06:19 -0500

    working screen keyboard

--- app.js
+++ app.js
@@ -7,13 +7,18 @@
 4. keyboard must also reflect the colors of tiles (gray, green, yellow)
 */
 document.addEventListener("DOMContentLoaded", function () {
+  //word bank//
+  const words = ["apple", "berry", "cherry", "date", "elder", "fig", "grape"];
+  const targetWord = words[Math.floor(Math.random() * words.length)];
+  console.log(words);
+
   //Keyboard that will allow you to place letters into boxes, delete, press enter//
-  var tiles = document.querySelectorAll(".tile");
-  var keys = document.querySelectorAll(".key");
+  const tiles = document.querySelectorAll(".tile");
+  const keys = document.querySelectorAll(".key");
   keys.forEach(function (key) {
     key.addEventListener("click", function () {
-      var letter = key.innerText;
-      for (var i = 0; i < tiles.length; i++) {
+      const letter = key.innerText;
+      for (let i = 0; i < tiles.length; i++) {
         if (!tiles[i].innerText) {
           tiles[i].innerText = letter;
           break;
