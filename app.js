3820c9b4e90e54fbd09aa91a6e89c6dde3e1f2df
Jahdai <jahdai.kilkenny@gmail.com>
2023-12-22 15:13:41 -0500

    letter comparison colors work!!!!!

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //HTML Variables
 const tiles = document.querySelectorAll(".tile"); //accesses all tiles//
 const keys = document.querySelectorAll(".key"); //accesses all keys//
+const playButton = document.getElementById("play");
+const modal = document.getElementById("welcome-modal");
 
 //Game Logic Variables
 const wordSize = 5; //set standard length for words//
 let currentRound = 1; //round user is currently on//
+
 import { words } from "./words.js"; //word bank of winning words, importing from additional js file//
 const winningWord = words[Math.floor(Math.random() * words.length)].split("");
 console.log(winningWord); // randomly pulls a word, thenadds to a array returning single letter strings...example ['b','e','r','r','y']
@@ -47,85 +50,39 @@ for (let key of keys) {
 
 //Play again function//
 
+//hide modal and start game
+// playButton.addEventListener('click',
 
+//
 
-//This creates functionality to compare letters and turn tiles/keys to correlating colors
+//compare tiles function
 
-/* 1. If letters in guess are = to winword && in the right position turn correlating tiles green. look at indexes 
-if winningWord[0]===playerGuess[0]||playerGuess[2]||etc{
-    background color green} 
-    
-2. /* 1. If letters in guess are = to winword && in the wrong position turn correlating tiles yellow. look at indexes 
-if winningWord[i]===playerGuess[1]||playerGuess[2]||etc{
-    background color yellow} */
+let compareLetters = () => {
+  const currentRow = document.querySelector(`.row-${currentRound}`);
 
-
-
-// let compareLetters = () => {
-//     if(winningWord[0]=== playerGuess[1]|| playerGuess[2]|| playerGuess[3]||playerGuess[4]||playerGuess[5]){
-
-// //turn background color yellow
-//     }
-
-// }
-
-// let compareLetters = () => {
-//   const currentRow = document.querySelector(`.row-${currentRound}`);
-
-//   for (let i = 0; i < playerGuess.length; i++) {
-//     if (winningWord[i] === playerGuess[i]) {
-//       // Turn background color yellow for the matching letter at index i
-//       const tile = currentRow.children[i];
-//       tile.style.backgroundColor = "green";
-//     } else if (winningWord[0] === playerGuess[i]) {
-//       // Turn background color yellow for the matching letter at index i
-//       const tile = currentRow.children[i];
-//       tile.style.backgroundColor = "yellow";
-//     }
-//   }
-// };
-
-// document.getElementById("testButton").addEventListener("click", () => {
-//   // Manually call compareLetters for testing
-//   compareLetters();
-// });
-
+  for (let i = 0; i < playerGuess.length; i++) { //loop through array
+    if (
+      winningWord[i] !== playerGuess[i] && //in word but wrong spot
+      winningWord.includes(playerGuess[i])
+    ) {
+      const tile = currentRow.children[i];
+      tile.style.backgroundColor = "orange"; 
+    } else {
+      if (winningWord[i] === playerGuess[i]) {
+        //letter is in same spot
+        const tile = currentRow.children[i];
+        tile.style.backgroundColor = "green"; //exact placement
+      } else {
+        const tile = currentRow.children[i];
+        tile.style.backgroundColor = "gray"; //not in word
+      }
+    }
+  }
+  document.getElementById("testButton").addEventListener("click", () => {
+    // Manually call compareLetters for testing
+    compareLetters();
+  });
+};
 compareLetters();
 
-//2nd determine if user guess has same letters in winning word
-//something like: let usersGuess = [''] (accept letters as items in array...thus giving it a position)
-//compare userguess item position to winningWord
-//Keyboard that will allow you to place letters into boxes, delete, press enter//
-
-// for (let key of keys) {
-//   key.addEventListener("click", () => {
-//     //find the next empty tile
-//     //when to move on to next row (listen for enter the if userWord=winningWord...move to new row)
-//     if (key.id === "enter") {
-//       if (playerGuess.join("") === winningWord.join("")) {
-//         alert("You Won!");
-//       } else if (currentRound < 6) {
-//         currentRound++;
-//         playerGuess = [];
-//       } else {
-//         alert(`You Lose the word was ${winningWord.join("")}`);
-//       }
-//     } else if (key.id === "delete") {
-//       //remove the last letter entry
-//     } else {
-//       if (playerGuess.length === 5) {
-//         alert("Do you want to submit? Press Enter"); //prompt method do you want to submit/or a modal where if they say okay, submit, then the enterkey will be clicked automatically for them. delay afte 10 seconds no manual enter
-//       } else {
-//         const currentRow = document.querySelector(`.row-${currentRound}`);
-//         playerGuess.push(key.id);
-//         currentRow.children[playerGuess.length - 1].innerText = key.innerText;
-//       }
-//     }
-//   });
-// }
-
-//make play button
-//change tile color
-//make delete key
-//play again
-//rules module lol
+//if
